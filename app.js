0557bf125d571b0bc1a7bb5f06a171aa68e01cfd
tophat8855 <hrfarley@gmail.com>
2015-01-20 13:41:07 -0800

    fixed bug, but had to de-factor. also added another owlbear

--- app.js
+++ app.js
@@ -82,21 +82,208 @@ function loadDungeonMap() {
   dungeonMap.mapTypes.set('map_styles_dungeon', styled_dungeon);
   dungeonMap.setMapTypeId('map_styles_dungeon');
   dungeonMap.overlayMapTypes.insertAt(0, hexagonalOverlay);
-  loadEncounters();
+  loadOwlbear();
+  loadOwlbear2();
+  loadPub();
+  loadDungeonEntrance();
 }
 
-function loadEncounters() {
-  var owlbear = new Marker('Owlbear', 49.791683, 9.940143, 'icon_owlbear.png', 'owlbear.png', 105);
-  var pub = new Marker('Pub', 49.790951, 9.942430, 'icon_pub.png', 'pub.png', 109);
-  var dungeon = new Marker('Dungeon', 49.792657, 9.939437, 'icon_dungeon.png', 'entrance.png', 102);
+function loadOwlbear2() {
+  var encounterPositionOwlbear = new google.maps.LatLng(49.792090, 9.941892);
+  var encounterIconOwlbear = {
+    url: 'images/icon_owlbear.png',
+    size: new google.maps.Size(16, 16),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(8, 8),
+  };
 
+  encounterOwlbear = new google.maps.Marker({
+    position: encounterPositionOwlbear,
+    map: dungeonMap,
+    title: 'Owlbear',
+    icon: encounterIconOwlbear,
+    zIndex:102,
+  });
 
-  google.maps.event.addListener(dungeonMap, "zoom_changed", function() {
-    var newZoom = dungeonMap.getZoom();
-    if (newZoom > 12) {
-      dungeonMap.setMapTypeId('map_styles_dungeon_zoomed');
-    } else {
-      dungeonMap.setMapTypeId('map_styles_dungeon');
-    }
+  var boxTextOwlbear = document.createElement("DIV");
+  boxTextOwlbear.style.cssText = pop_up_info;
+  boxTextOwlbear.innerHTML = '<span class="pop_up_box_text"><img src="images/owlbear.png" width="100" height="75" border="0" /></span>';
+
+  var infoboxOptionsOwlbear = {
+    content: boxTextOwlbear,
+    disableAutoPan: false,
+    maxWidth: 0,
+    pixelOffset: new google.maps.Size(-50, 0),
+    zIndex:null,
+    boxStyle:{
+      background: "url('infobox/pop_up_box_top_arrow.png') no-repeat",
+      opacity: 1,
+      width: "150px"
+    },
+    closeBoxMargin: "2px 2px 2px 2px",
+    closeBoxURL: "images/button_close.png",
+    infoBoxClearance: new google.maps.Size(1, 1),
+    isHidden: false,
+    pane: "floatPane",
+    enableEventPropagation: false
+  };
+
+  infoboxOwlbear = new InfoBox(infoboxOptionsOwlbear);
+
+  google.maps.event.addListener(encounterOwlbear, "click", function(e) {
+    infoboxOwlbear.open(dungeonMap, this);
+    this.setZIndex(google.maps.Marker.MAX_ZINDEX + 1);
+    setZoomWhenEncounterClicked();
+    dungeonMap.setCenter(encounterOwlbear.getPosition());
   });
 }
+
+function loadOwlbear() {
+  var encounterPositionOwlbear = new google.maps.LatLng(49.791683, 9.940143);
+  var encounterIconOwlbear = {
+    url: 'images/icon_owlbear.png',
+    size: new google.maps.Size(16, 16),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(8, 8),
+  };
+
+  encounterOwlbear = new google.maps.Marker({
+    position: encounterPositionOwlbear,
+    map: dungeonMap,
+    title: 'Owlbear',
+    icon: encounterIconOwlbear,
+    zIndex:102,
+  });
+
+  var boxTextOwlbear = document.createElement("DIV");
+  boxTextOwlbear.style.cssText = pop_up_info;
+  boxTextOwlbear.innerHTML = '<span class="pop_up_box_text"><img src="images/owlbear.png" width="100" height="75" border="0" /></span>';
+
+  var infoboxOptionsOwlbear = {
+    content: boxTextOwlbear,
+    disableAutoPan: false,
+    maxWidth: 0,
+    pixelOffset: new google.maps.Size(-50, 0),
+    zIndex:null,
+    boxStyle:{
+      background: "url('infobox/pop_up_box_top_arrow.png') no-repeat",
+      opacity: 1,
+      width: "150px"
+    },
+    closeBoxMargin: "2px 2px 2px 2px",
+    closeBoxURL: "images/button_close.png",
+    infoBoxClearance: new google.maps.Size(1, 1),
+    isHidden: false,
+    pane: "floatPane",
+    enableEventPropagation: false
+  };
+
+  infoboxOwlbear = new InfoBox(infoboxOptionsOwlbear);
+
+  google.maps.event.addListener(encounterOwlbear, "click", function(e) {
+    infoboxOwlbear.open(dungeonMap, this);
+    this.setZIndex(google.maps.Marker.MAX_ZINDEX + 1);
+    setZoomWhenEncounterClicked();
+    dungeonMap.setCenter(encounterOwlbear.getPosition());
+  });
+}
+
+function loadPub() {
+  var encounterPositionPub = new google.maps.LatLng(49.790951, 9.942430);
+  var encounterIconPub = {
+    url: 'images/icon_pub.png',
+    size: new google.maps.Size(19, 23),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(10, 10),
+  };
+
+  encounterPub = new google.maps.Marker({
+    position: encounterPositionPub,
+    map: dungeonMap,
+    title: 'Pub',
+    icon: encounterIconPub,
+    zIndex:104,
+  });
+
+  var boxTextPub = document.createElement("DIV");
+  boxTextPub.style.cssText = pop_up_info;
+  boxTextPub.innerHTML = '<span class="pop_up_box_text"><img src="images/pub.png" width="100" height="100" border="0" /></span>';
+
+  var infoboxOptionsPub = {
+    content: boxTextPub,
+    disableAutoPan: false,
+    maxWidth: 0,
+    pixelOffset: new google.maps.Size(-50, 0),
+    zIndex:null,
+    boxStyle:{
+      background: "url('infobox/pop_up_box_top_arrow.png') no-repeat",
+      opacity: 1,
+      width: "150px"
+    },
+    closeBoxMargin: "2px 2px 2px 2px",
+    closeBoxURL: "images/button_close.png",
+    infoBoxClearance: new google.maps.Size(1, 1),
+    isHidden: false,
+    pane: "floatPane",
+    enableEventPropagation: false
+  };
+
+  infoboxPub = new InfoBox(infoboxOptionsPub);
+
+  google.maps.event.addListener(encounterPub, "click", function(e) {
+    infoboxPub.open(dungeonMap, this);
+    this.setZIndex(google.maps.Marker.MAX_ZINDEX + 1);
+    setZoomWhenEncounterClicked();
+    dungeonMap.setCenter(encounterPub.getPosition());
+  });
+}
+
+  function loadDungeonEntrance() {
+    var encounterPositionDungeon = new google.maps.LatLng(49.792657, 9.939437);
+    var encounterIconDungeon = {
+      url: 'images/icon_dungeon.png',
+      size: new google.maps.Size(32, 32),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(16, 16),
+    };
+
+    encounterDungeon = new google.maps.Marker({
+      position: encounterPositionDungeon,
+      map: dungeonMap,
+      title: 'Pub',
+      icon: encounterIconDungeon,
+      zIndex:104,
+    });
+
+    var boxTextDungeon = document.createElement("DIV");
+    boxTextDungeon.style.cssText = pop_up_info;
+    boxTextDungeon.innerHTML = '<span class="pop_up_box_text"><img src="images/entrance.png" width="64" height="64" border="0" /></span>';
+
+    var infoboxOptionsDungeon = {
+      content: boxTextDungeon,
+      disableAutoPan: false,
+      maxWidth: 0,
+      pixelOffset: new google.maps.Size(-50, 0),
+      zIndex:null,
+      boxStyle:{
+        background: "url('infobox/pop_up_box_top_arrow.png') no-repeat",
+        opacity: 1,
+        width: "150px"
+      },
+      closeBoxMargin: "2px 2px 2px 2px",
+      closeBoxURL: "images/button_close.png",
+      infoBoxClearance: new google.maps.Size(1, 1),
+      isHidden: false,
+      pane: "floatPane",
+      enableEventPropagation: false
+    };
+
+    infoboxDungeon = new InfoBox(infoboxOptionsDungeon);
+
+    google.maps.event.addListener(encounterDungeon, "click", function(e) {
+      infoboxDungeon.open(dungeonMap, this);
+      this.setZIndex(google.maps.Marker.MAX_ZINDEX + 1);
+      setZoomWhenEncounterClicked();
+      dungeonMap.setCenter(encounterDungeon.getPosition());
+    });
+  }
